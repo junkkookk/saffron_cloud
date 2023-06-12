1680df8767d944acdc15b4570950986943c2853f
junkkookk <xiaobai8871@gmail.com>
2023-06-12 11:38:20 +0800

    feat: 删除mongo

--- saffron-server/src/main/java/com/w/saffron/listener/AppStartListener.java
+++ saffron-server/src/main/java/com/w/saffron/listener/AppStartListener.java
@@ -1,13 +1,10 @@
 package com.w.saffron.listener;
 
-import com.w.saffron.application.sys.domain.Config;
 import com.w.saffron.schdule.JobManager;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -20,29 +17,13 @@ import java.util.List;
 @Component
 public class AppStartListener implements ApplicationListener<ApplicationStartedEvent> {
 
-    @Autowired
-    MongoTemplate mongoTemplate;
-
     @Override
     public void onApplicationEvent(@NotNull ApplicationStartedEvent event) {
         initJob();
-        initConfig();
     }
 
     private void initJob() {
         JobManager.addJob(List.of(
         ));
-    }
-
-    public void initConfig(){
-        Config config = mongoTemplate.findById(1L, Config.class);
-        if (config==null){
-            config = new Config();
-            config.setId(1L);
-            config.setBackgroundImage("/saffron_files/background.jpg");
-            config.setAppName("saffron");
-            config.setAppDesc("a saffron app");
-            mongoTemplate.save(config);
-        }
     }
 }
